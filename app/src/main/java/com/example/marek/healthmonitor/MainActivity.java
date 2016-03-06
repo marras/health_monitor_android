8def5b475d94e88ad9a1a1382b2761fa14d12f47
Marek Waligórski <marek.waligorski@netguru.pl>
2016-03-06 19:39:35 +0100

    use JSONs + send login info in request

--- app/src/main/java/com/example/marek/healthmonitor/MainActivity.java
+++ app/src/main/java/com/example/marek/healthmonitor/MainActivity.java
@@ -6,6 +6,8 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
@@ -25,72 +27,156 @@ import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.protocol.HTTP;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.concurrent.ExecutionException;
 
 
 public class MainActivity extends ActionBarActivity {
-    HttpClient client = new DefaultHttpClient();
-    HttpPost post = new HttpPost("http://health-monit.herokuapp.com/data");
-
-    ArrayList<String> metrics = new ArrayList<String>();
+    ArrayList<JSONObject> metrics = new ArrayList<JSONObject>();
     int metricIndex = 0;
+    int userId = -1;
 
-    public class UploadTask extends AsyncTask<String, Void, String> {
+    public class UploadTask extends AsyncTask<JSONObject, Void, String> {
 
         private Exception exception = null;
 
         @Override
-        protected String doInBackground(String... params) {
+        protected String doInBackground(JSONObject... params) {
             try {
-                String postMessage = "{\"user\": \"" + params[2] + "\", \"values\": { \"" + params[0]+ "\": " + params[1] + "} }";
-                Log.i("PostData", "Post message: '"+postMessage+"'");
+                String postMessage = params[0].toString();
+                Log.i("PostData", "Post message: '" + postMessage + "'");
+
+                Uri.Builder builder = new Uri.Builder();
+                builder.scheme("http")
+                        .authority("health-monit.herokuapp.com")
+                        .appendPath("data")
+                        .appendQueryParameter("session[name]", params[0].getJSONObject("session").getString("name"))
+                        .appendQueryParameter("session[password]", params[0].getJSONObject("session").getString("password"));
+                String url = builder.build().toString();
+                Log.i("URL:", url);
+
+                HttpPost post = new HttpPost(url);
                 post.setHeader(HTTP.CONTENT_TYPE, "application/json; charset=UTF-8");
                 post.setEntity(new ByteArrayEntity(postMessage.getBytes("UTF8")));
-             } catch (UnsupportedEncodingException ex) {
+
+                Log.i("PostData", "Executing request...");
+                HttpClient client = new DefaultHttpClient();
+                HttpResponse response = client.execute(post);
+                Log.i("PostData - Response", response.getStatusLine().toString());
+            } catch (JSONException|IOException ex) {
                 this.exception = ex;
-                return null;
             }
 
-            Log.i("PostData", "Executing request...");
-            try {
-                HttpResponse response = client.execute(post);
-            } catch (IOException ex) {
-                this.exception = ex;
-                return null;
-            }
             return null;
         }
 
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
-            Log.i("PostData", "Post-execute - fuck this shit");
             if (this.exception != null) Log.e("PostData", this.exception.getMessage());
+        }
+    }
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        private Exception exception = null;
+
+        // (name, password)
+        @Override
+        protected String doInBackground(String... params) {
+            String result = "";
+            URL url;
+            HttpURLConnection connection = null;
+
+            try {
+                Uri.Builder builder = new Uri.Builder();
+                builder.scheme("http")
+                        .authority("health-monit.herokuapp.com")
+                        .appendPath("metrics")
+                        .appendQueryParameter("session[name]", params[0])
+                        .appendQueryParameter("session[password]", params[1]);
+                url = new URL(builder.build().toString());
+
+                connection = (HttpURLConnection) url.openConnection();
+                InputStream is = connection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(is);
+
+                int data = reader.read();
+
+                while (data != -1) {
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+                }
+
+                Log.i("Download", "Result: '"+result+"'");
+                return result;
+            } catch (Exception ex) {
+                this.exception = ex;
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            if (this.exception != null) {
+                this.exception.printStackTrace();
+            } else {
+                try {
+                    Log.i("Download SUCCESS:", result);
+                    JSONObject jObject = new JSONObject(result);
+                    userId = jObject.getInt("user");
+                    JSONArray array = jObject.getJSONArray("metrics");
+                    for (int i = 0; i < array.length(); i++) {
+                        metrics.add(array.getJSONObject(i));
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
     public void postData(View view) {
         Log.i("PostData", "starting...");
-        String tag = view.getTag().toString();
-        String user = PreferenceManager.getDefaultSharedPreferences(this).getString("username", "");
-        String metric = metrics.get(metricIndex);
-
-        Log.i("Settings user:", user);
-
-        UploadTask task = new UploadTask();
         try {
-            task.execute(metric, tag, user).get();
-        } catch(InterruptedException e) {
-            e.printStackTrace();
-        } catch(ExecutionException e) {
+            String tag = view.getTag().toString();
+            String metric_name = metrics.get(metricIndex).getString("name");
+
+            UploadTask task = new UploadTask();
+
+            JSONObject data = new JSONObject();
+            JSONObject sessionData = new JSONObject();
+            JSONObject valuesData = new JSONObject();
+            sessionData.put("name", getPrefs("username"));
+            sessionData.put("password", getPrefs("password"));
+            valuesData.put(metric_name, tag);
+            data.put("session", sessionData);
+            data.put("values", valuesData);
+
+            task.execute(data).get();
+        } catch(InterruptedException|ExecutionException|JSONException e) {
             e.printStackTrace();
         }
         Toast.makeText(getApplicationContext(), "I/O failed, fuck you!", Toast.LENGTH_LONG);
+    }
+
+    private String getPrefs(String key) {
+        return PreferenceManager.getDefaultSharedPreferences(this).getString(key, "");
     }
 
     public void launchSettings (View view) {
@@ -98,19 +184,17 @@ public class MainActivity extends ActionBarActivity {
         startActivity(i);
     }
 
-    Waker waker = new Waker();
-
-    public void setAlarms(View view) {
-        waker.SetAlarm(this, 22, 40);
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // GET this from internet
-        metrics.add("Head");
+        DownloadTask task = new DownloadTask();
+        try {
+            task.execute(getPrefs("username"), getPrefs("password")).get();
+        } catch(InterruptedException|ExecutionException e) {
+            e.printStackTrace();
+        }
     }
 
 
